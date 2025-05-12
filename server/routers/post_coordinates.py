ac97dc009935b7d1be1542f2eac03cbb982e705f
klemiere <lemierekevin1@gmail.com>
2025-05-12 14:58:18 +0200

    Update error handling

--- server/routers/post_coordinates.py
+++ server/routers/post_coordinates.py
@@ -1,15 +1,37 @@
-from fastapi import APIRouter
-from pydantic import BaseModel
+from models import Position
+from schemas import Coordinates
+from database import SessionLocal
+from fastapi import APIRouter, HTTPException
+from sqlalchemy.exc import SQLAlchemyError
+
 
 router = APIRouter()
 
-class Coordinates(BaseModel):
-    latitude: str
-    longitude: str
-    datetime: str
-    vehicule: str
-
 @router.post("/post_coordinates")
-async def create_post(payload: Coordinates):
-    print(payload)
-    return payload
+async def post_request(payload: Coordinates):
+    
+    session = SessionLocal()
+
+    position_data = Position(
+        position_timestamp = payload.timestamp,
+        position_lat = payload.latitude,
+        position_long = payload.longitude,
+        tracker_id=payload.tracker_id
+    )
+
+    try:
+        session.add(position_data)
+        session.commit()
+        return {"message": "Position data inserted successfully."}
+        
+    except SQLAlchemyError as e:
+        session.rollback()
+        raise HTTPException(status_code=500, detail=f"Database error: {str(e)}")
+
+    except Exception as e:
+        session.rollback()
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
+    
+    finally:
+        session.close()
+
