2f3450615f9444963a42a4f59039eabd24bfdbaa
klemiere <lemierekevin1@gmail.com>
2025-05-13 11:45:33 +0200

    Implement post_new_user route

---
+++ server/routers/post_user.py
@@ -0,0 +1,34 @@
+from models import Users
+from schemas import User_schema
+from database import SessionLocal
+from fastapi import APIRouter, HTTPException
+from sqlalchemy.exc import SQLAlchemyError
+
+
+router = APIRouter()
+
+@router.post("/post_new_user")
+async def post_new_user(payload: User_schema):
+    
+    session = SessionLocal()
+
+    user_data = Users(
+        username = payload.username,
+        password = payload.password
+    )
+
+    try:
+        session.add(user_data)
+        session.commit()
+        return {"message": "User data inserted successfully."}
+        
+    except SQLAlchemyError as e:
+        session.rollback()
+        raise HTTPException(status_code=500, detail=f"Database error: {str(e)}")
+
+    except Exception as e:
+        session.rollback()
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
+    
+    finally:
+        session.close()
