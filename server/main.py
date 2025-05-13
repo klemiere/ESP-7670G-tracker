f00e6bd4c83b78e319b972958ce35004ce2b27e0
klemiere <lemierekevin1@gmail.com>
2025-05-13 11:46:03 +0200

    Add new routers

--- server/main.py
+++ server/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
-from routers.post_coordinates import router as post_coordinates_router
-from routers.get_coordinates import router as get_coordinates_router
+from routers.get_coordinates_single_tracker import router as get_coordinates_single_tracker_router
+from routers.get_coordinates_all_trackers import router as get_coordinates_all_trackers_router
 from routers.get_coordinates_time_range import router as get_coordinates_time_range_router
+from routers.get_user import router as get_user_router
+from routers.post_coordinates import router as post_coordinates_router
+from routers.post_user import router as post_new_user_router
 
 server = FastAPI()
 
@@ -10,6 +13,9 @@ server = FastAPI()
 async def root():
     return RedirectResponse(url="/docs")
 
-server.include_router(get_coordinates_router)
-server.include_router(post_coordinates_router)
+server.include_router(get_coordinates_single_tracker_router)
+server.include_router(get_coordinates_all_trackers_router)
 server.include_router(get_coordinates_time_range_router)
+server.include_router(get_user_router)
+server.include_router(post_coordinates_router)
+server.include_router(post_new_user_router)
