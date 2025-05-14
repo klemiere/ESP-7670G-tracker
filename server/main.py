f9f03549ab719b18190b0f36e36249ae625ce27b
klemiere <lemierekevin1@gmail.com>
2025-05-14 11:13:50 +0200

    Implement all the new routers

--- server/main.py
+++ server/main.py
@@ -4,8 +4,11 @@ from routers.get_coordinates_single_tracker import router as get_coordinates_sin
 from routers.get_coordinates_all_trackers import router as get_coordinates_all_trackers_router
 from routers.get_coordinates_time_range import router as get_coordinates_time_range_router
 from routers.get_user import router as get_user_router
+from routers.get_trackers import router as get_trackers_router
+from routers.get_vehicles import router as get_vehicles_routers
 from routers.post_coordinates import router as post_coordinates_router
-from routers.post_user import router as post_new_user_router
+from routers.post_add_user import router as post_add_user_router
+from routers.post_add_tracker import router as post_add_tracker_router
 
 server = FastAPI()
 
@@ -17,5 +20,8 @@ server.include_router(get_coordinates_single_tracker_router)
 server.include_router(get_coordinates_all_trackers_router)
 server.include_router(get_coordinates_time_range_router)
 server.include_router(get_user_router)
+server.include_router(get_trackers_router)
+server.include_router(get_vehicles_routers)
 server.include_router(post_coordinates_router)
-server.include_router(post_new_user_router)
+server.include_router(post_add_user_router)
+server.include_router(post_add_tracker_router)
