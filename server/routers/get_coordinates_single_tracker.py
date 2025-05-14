4fef87c8d15d44dffc45f40d2c1befb047b95659
klemiere <lemierekevin1@gmail.com>
2025-05-14 15:09:10 +0200

    Update param to tracker_identifier

--- server/routers/get_coordinates_single_tracker.py
+++ server/routers/get_coordinates_single_tracker.py
@@ -1,28 +1,37 @@
-from models import Positions
 from database import SessionLocal
+from models import Positions, Trackers
+from schemas import CoordinatesResponse
 from fastapi import APIRouter, Query, HTTPException
+from typing import List
 from sqlalchemy.exc import SQLAlchemyError
 
 
 router = APIRouter()
 
-@router.get("/get_coordinates_single_tracker")
+@router.get("/get_coordinates_single_tracker", response_model=List[CoordinatesResponse])
 async def get_positions_single_tracker(
-    tracker_id: int = Query(..., title="tracker_id",
-                            description="ID of the tracker to query for."), 
+    tracker_identifier: str = Query(..., title="Tracker identifier",
+                            description="The tracker's identifier"), 
     limit: int = Query(1, title="limit",
-                       description="Amount of records to return.")):
+                       description="Amount of records to return."),):
     
     session = SessionLocal()
 
+    tracker = session.query(Trackers).filter(Trackers.tracker_identifier == tracker_identifier).first()
+    if not tracker:
+        raise HTTPException(status_code=404, detail=f"tracker: {tracker_identifier} not found")
+
     try:
         # Query positions, sort by timestamp descending, retrieve a "limit" amount of datapoints and put them in a "position" list
-        positions = session.query(Positions).filter(Positions.tracker_id == tracker_id).order_by(Positions.position_timestamp.desc()).limit(limit).all()
+        positions = session.query(Positions).filter(Positions.tracker_id == tracker.tracker_id).order_by(Positions.position_timestamp.desc()).limit(limit).all()
 
         if not positions:
-            return {"message": "No position found for the given tracker ID."}
+            raise HTTPException(status_code=404, detail=f"No positions for {tracker_identifier} found")
         
         return positions
+    
+    except HTTPException as e:
+        raise e
 
     except SQLAlchemyError as e:
         raise HTTPException(status_code=500, detail=f"Database error: {str(e)}")
