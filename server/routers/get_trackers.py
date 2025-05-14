9089b8c31f1e4f68680e2eb5a34e838dae44ee12
klemiere <lemierekevin1@gmail.com>
2025-05-14 11:02:08 +0200

    Add and implement get_trackers method

---
+++ server/routers/get_trackers.py
@@ -0,0 +1,37 @@
+from models import Trackers
+from database import SessionLocal
+from fastapi import APIRouter, Query, HTTPException
+from sqlalchemy.exc import SQLAlchemyError
+
+
+router = APIRouter()
+
+@router.get("/get_trackers")
+async def get_trackers(
+    tracker_id: int = Query(0, title="Tracker ID")):
+    
+    session = SessionLocal()
+
+    try:
+        # If tracker_id = 0 return all trackers
+        if tracker_id == 0:
+            trackers = session.query(Trackers).all()
+
+            return trackers
+        
+        else:
+            tracker = session.query(Trackers).filter(Trackers.tracker_id == tracker_id).first()
+
+            if not tracker:
+                return {"message": "No tracker with this ID found."}
+            
+            return tracker
+
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail=f"Database error: {str(e)}")
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
+
+    finally:
+        session.close()
