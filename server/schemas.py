fb82e8b8fc7bc81566c131dbdeccba4028e567a2
klemiere <lemierekevin1@gmail.com>
2025-05-14 14:27:35 +0200

    Add TrackerResponse, VehicleResponse and AddVehicleToTrackerSchema schemas

--- server/schemas.py
+++ server/schemas.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
+from typing import Optional
 from decimal import Decimal
-from enum import Enum
+from models import TrackerTypeEnum
 
 class CoordinatesSchema(BaseModel):
     tracker_identifier: str
@@ -14,4 +15,27 @@ class UserSchema(BaseModel):
 
 class TrackerSchema(BaseModel):
     tracker_identifier: str
-    tracker_type: str
+    tracker_type: TrackerTypeEnum
+
+class TrackerResponse(BaseModel):
+    tracker_id: int
+    tracker_identifier: str
+    tracker_type: TrackerTypeEnum
+    tracker_on_site: bool
+    tracker_battery_low: bool
+    vehicle_id: Optional[int] = None
+
+    class Config:
+        orm_mode = True
+
+class VehicleResponse(BaseModel):
+    vehicle_id: int
+    vehicle_plate: str
+
+    class Config:
+        orm_mode = True
+
+
+class AddVehicleToTrackerSchema(BaseModel):
+    tracker_identifier: str
+    vehicle_plate: str
